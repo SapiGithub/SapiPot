f73b64a87f1f6dd25e44a2334b4577959dfa773b
NashriSP <71427374+NashriSP@users.noreply.github.com>
2023-07-05 18:20:25 +0700

    Update sniff.py

--- TheSapiPot/sniff.py
+++ TheSapiPot/sniff.py
@@ -1,24 +1,14 @@
 from scapy.all import *
 from TheSapiPot.filter import is_host
-import threading
 
 class Sniffer:
-    def __init__(self, prn=None, interface=None,host_ip=None):
-        self.host_ip=host_ip
+    def __init__(self, prn=None, interface=None, host_ip=None):
+        self.host_ip = host_ip
         self.interface = interface
-        if prn:
-            self.prn = prn
-        else:
-            self.prn = lambda p: f"{p.summary()}"
-        
-        # self.protocols = ['tcp','arp','udp']
-        # self.thread = {}
-        
-    # def run(self):
-    #     for protocol in self.protocols:
-    #         self.thread[protocol] = threading.Thread(target=self._sniff, args=(protocol,))
-    #         self.thread[protocol].start()
+        self.prn = prn or (lambda p: f"{p.summary()}")
+
+    def packet_filter(self, packet):
+        return is_host(packet, self.host_ip)
 
     def run(self):
-        packet_filter = lambda p:is_host(p, self.host_ip)
-        sniff(prn=self.prn, iface=self.interface,lfilter=packet_filter,store='False')
+        sniff(prn=self.prn, iface=self.interface, lfilter=self.packet_filter, store=False)
