a727b2ad3965bb8c644bbf3d88cb65db325f2be4
NashriSP <71427374+NashriSP@users.noreply.github.com>
2023-07-05 19:12:54 +0700

    Update sniffDir.py

--- TheSapiPot/sniffDir.py
+++ TheSapiPot/sniffDir.py
@@ -1,4 +1,5 @@
 import os
+import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -8,20 +9,21 @@ class FolderMonitor(FileSystemEventHandler):
         super().__init__()
         self.monitor_folder = monitor_folder
         self.logger = log_file
-
+        self.is_directory = lambda arg: "Directory" if arg else "File"
+        
     def on_any_event(self, event):
         self.logger.info(f'[File Monitor]\n[*]Event Type: {event.event_type}\n[*]Target: {event.src_path}\n[*]Target Type: {self.is_directory(event.is_directory)}')
 
-
 def start_monitoring(monitor_folder, log_file):
-    monitor_folder = os.path.expanduser(monitor_folder)
     monitor = FolderMonitor(monitor_folder, log_file)
+    observer = Observer()
+    observer.schedule(monitor, os.path.expanduser(monitor_folder), recursive=True)
+    observer.start()
 
-    with Observer() as observer:
-        observer.schedule(monitor, monitor_folder, recursive=True)
-        observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
 
-        try:
-            observer.join()
-        except KeyboardInterrupt:
-            observer.stop()
+    observer.join()
