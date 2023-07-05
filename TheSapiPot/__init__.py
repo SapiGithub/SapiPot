a1dde633ca26b75825dd040a0eb3fd90b1b1ddf0
NashriSP <71427374+NashriSP@users.noreply.github.com>
2023-07-05 18:18:17 +0700

    Update __init__.py

--- TheSapiPot/__init__.py
+++ TheSapiPot/__init__.py
@@ -1,20 +1,21 @@
 import logging
+import signal
 from scapy.all import *
-import scapy.all as scapy
 from scapy.layers.inet import IP, TCP
 from scapy.layers.http import HTTPRequest
+from TheSapiPot.packetHTTP import modelHTTP
 from TheSapiPot.sniff import Sniffer
 from TheSapiPot.packetARP import check_MTIM
-from TheSapiPot.packetHTTP import modelHTTP
 from TheSapiPot.sniffDir import start_monitoring
 
-class HoneyPot(object):
-    def __init__(self,host,interface,dirfile,logfile):
+class HoneyPot:
+    def __init__(self, host, interface, dirfile, logfile):
         self.host = host
         self.interface = interface
         self.dirfile = dirfile
         self.logfile = logfile
-        
+        self.stop = False
+
         logging.basicConfig(
             level=logging.DEBUG,
             format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -27,22 +28,19 @@ class HoneyPot(object):
         logging.getLogger('watchdog.observers.inotify_buffer').setLevel(logging.WARNING)
         self.logger.info(f'[*] logfile: {self.logfile}')
         self.logger.info("[*] HoneyPot Initializing....... ")
-    
-    def logging_packet(self,packet: Packet):
+
+    def logging_packet(self, packet: Packet):
         if packet.haslayer(TCP):
             ip = packet[IP]
             tcp = packet[TCP]
-            flags = tcp.flags
-            if (packet.haslayer(HTTPRequest)and ip.dst == self.host):
+            if packet.haslayer(HTTPRequest) and ip.dst == self.host:
                 prd = modelHTTP(packet)
                 if prd:
                     if packet.haslayer(Raw):
-                        self.logger.info(f"[HTTP Attack]\n[*]Packet Summary: {packet.summary()}\n[*]Packet Payload: {packet[Raw].load.decode()}\n[*]AI Prediction: \n{prd.predicts()}\n")  
-                    else:  
-                        self.logger.info(f"[HTTP Attack]\n[*]Packet Summary: {packet.summary()}\n[*]Packet Payload: {packet[HTTPRequest].Path.decode()}\n[*]AI Prediction: \n{prd.predicts()}\n") 
-                else:
-                    pass 
-            elif (flags in ["RA" ,"R", "FA", "F"]) and not ((tcp.dport in [80,8080,443] or tcp.sport in [80,8080,443])):
+                        self.logger.info(f"[HTTP Attack]\n[*]Packet Summary: {packet.summary()}\n[*]Packet Payload: {packet[Raw].load.decode()}\n[*]AI Prediction: \n{prd.predicts()}\n")
+                    else:
+                        self.logger.info(f"[HTTP Attack]\n[*]Packet Summary: {packet.summary()}\n[*]Packet Payload: {packet[HTTPRequest].Path.decode()}\n[*]AI Prediction: \n{prd.predicts()}\n")
+            elif flags in ["RA", "R", "FA", "F"] and not (tcp.dport in [80, 8080, 443] or tcp.sport in [80, 8080, 443]):
                 self.logger.info(f"[Port Scan]\n[*]Packet Summary: {packet.summary()}\n")
         elif packet.haslayer(UDP):
             try:
@@ -54,12 +52,23 @@ class HoneyPot(object):
         elif packet.haslayer(ARP):
             if check_MTIM(packet):
                 self.logger.info(f"[ARP SPOOF]\n[*]Packet Summary: {packet.summary()}\n")
-            else:
-                pass
 
+    def signal_handler(self, signum, frame):
+        self.stop = True
 
     def run(self):
-        print(f"[*] Filter: For IpAddress: {self.host}\n[*] Monitoring For Directory or File: {self.dirfile}")
-        sniffer = Sniffer(prn=self.logging_packet, interface=self.interface,host_ip=self.host)
-        sniffer.run()
-        start_monitoring(self.dirfile,self.logger)
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+        print(f"[*] Filter: For IPAddress: {self.host}\n[*] Monitoring For Directory or File: {self.dirfile}")
+        sniffer = Sniffer(prn=self.logging_packet, interface=self.interface, host_ip=self.host)
+        monitor_thread = threading.Thread(target=start_monitoring, args=(self.dirfile, self.logger))
+
+        sniffer_thread = threading.Thread(target=sniffer.run, args=(self.stop,))
+        monitor_thread.start()
+        sniffer_thread.start()
+
+        while not self.stop:
+            time.sleep(0.1)
+
+        sniffer_thread.join()
+        monitor_thread.join()
