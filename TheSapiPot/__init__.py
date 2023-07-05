af4cb977c51e7877c7b7d3c85e46847f5aa7f83f
NashriSP <71427374+NashriSP@users.noreply.github.com>
2023-07-05 19:49:25 +0700

    Update __init__.py

--- TheSapiPot/__init__.py
+++ TheSapiPot/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import signal
 from scapy.all import *
 from scapy.layers.inet import IP, TCP
 from scapy.layers.http import HTTPRequest
@@ -53,21 +54,26 @@ class HoneyPot:
             if check_MTIM(packet):
                 self.logger.info(f"[ARP SPOOF]\n[*]Packet Summary: {packet.summary()}\n")
 
+    def signal_handler(self, signum, frame):
+        self.stop = True
+
     def run(self):
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+        print(f"[*] Filter: For IPAddress: {self.host}\n[*] Monitoring For Directory or File: {self.dirfile}")
+        sniffer = Sniffer(prn=self.logging_packet, interface=self.interface, host_ip=self.host)
+        monitor_thread = threading.Thread(target=start_monitoring, args=(self.dirfile, self.logger))
+
+        sniffer_thread = threading.Thread(target=sniffer.run)
+        monitor_thread.start()
+        sniffer_thread.start()
+
         try:
-            print(f"[*] Filter: For IPAddress: {self.host}\n[*] Monitoring For Directory or File: {self.dirfile}")
-            sniffer = Sniffer(prn=self.logging_packet, interface=self.interface, host_ip=self.host)
-            monitor_thread = threading.Thread(target=start_monitoring, args=(self.dirfile, self.logger))
-        
-            sniffer_thread = threading.Thread(target=sniffer.run)
-            monitor_thread.start()
-            sniffer_thread.start()
-        
-            while not self.stop:
+            while True:
                 time.sleep(0.1)
         except KeyboardInterrupt:
             self.stop = True
-            sniffer_thread.join()
-            monitor_thread.join()
-            sys.exit(0)
+
+        sniffer_thread.join()
+        monitor_thread.join()
 
