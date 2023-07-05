9a918d50bbd3c688c2fb95665217302b155f59ff
NashriSP <71427374+NashriSP@users.noreply.github.com>
2023-07-05 12:49:32 +0700

    Update filter.py

--- TheSapiPot/filter.py
+++ TheSapiPot/filter.py
@@ -6,9 +6,9 @@ def is_host(packet: Packet, ip_address: str) -> bool:
     if packet.haslayer(TCP):
         i = packet[IP]
         return ip_address in (i.src, i.dst)
-    if packet.haslayer(ARP):
-        i = packet[ARP]
-        return ip_address in (i.pdst)
-    if packet.haslayer(UDP) and packet.haslayer(IP):
+    elif packet.haslayer(UDP) and packet.haslayer(IP):
         i = packet[IP]
         return ip_address in (i.dst)
+    elif packet.haslayer(ARP):
+        i = packet[ARP]
+        return ip_address in (i.pdst)
