669524bb95cae29326c8ab7e037a3736e72b8a8d
NashriSP <71427374+NashriSP@users.noreply.github.com>
2023-07-14 14:43:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ fi
 config_file="$1"
 nohup sudo python3 -m TheSapiPot "$config_file" > /dev/null 2>&1 &
 
-# Generate stop_script.sh
+# Generate stop.sh
 echo "#!/bin/bash" > stop_script.sh
 echo "" >> stop_script.sh
-echo "pkill -f \"python3 -m TheSapiPot $config_file\"" >> stop_script.sh
-chmod +x stop_script.sh
+echo "pkill -f \"python3 -m TheSapiPot $config_file\"" >> stop.sh
+chmod +x stop.sh
